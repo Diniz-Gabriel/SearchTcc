cc71d18edbf0ee5684461e599088b5cab1b12084
Jab1roca <gabriel.silva2499@etec.sp.gov.br>
2022-11-08 17:01:22 -0300

    telas de login funcionando quase prontas

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,33 @@
+ <!-- Comeco da menu -->
+ <header>
+        <nav>
+            <a class="logo"></a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>    
+            <ul class="nav-list">
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/">Sobre</a></li>
+                <li><a href="/">Projetos</a></li>
+                <li><a href="/">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <header class="header">
+    <img src="{{ asset('imagens\logo_search.png') }}" alt="Logo" height="300" width="300" draggable="false">
+    </header>
+    <script src="{{ asset('js\mobile_navbar.js') }}"></script>
+    <link href="{{ asset('css\home\stylemenu.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css\home\stylehome.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Final do menu -->
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="" />
             </a>
         </x-slot>
 
@@ -62,3 +87,30 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+         <!--Comeco do rodape -->
+         <footer>
+            <div class="waves">
+                <div class="wave" id="wave1"></div>
+                <div class="wave" id="wave2"></div>
+                <div class="wave" id="wave3"></div>
+                <div class="wave" id="wave4"></div>
+            </div>
+        <ul class="redessociais">
+                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+        </ul>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <p>Search</p>
+        </footer>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>  
+        <link href="{{ asset('css\loginEregister\footerregister.css') }}" rel="stylesheet" type="text/css">
+     <!--Final do rodape -->
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('st');
+    return view('register');
 });
 
 Route::get('/dashboard', function () {
