7e94a5405c91d9ea4c857dc407cf6f3bf9e9538b
Jab1roca <gabriel.silva2499@etec.sp.gov.br>
2022-12-13 17:33:24 -0300

    telas todas padronizadas

--- resources/views/st.blade.php
+++ resources/views/st.blade.php
@@ -11,24 +11,7 @@
     <title>Stranger Things</title>
 </head>
 <body>
-    <!-- Comeco da menu -->
-    <header>
-        <nav>
-            <a class="logo"></a>
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>    
-            <ul class="nav-list">
-                <li><a href="/">Inicio</a></li>
-                <li><a href="/">Sobre</a></li>
-                <li><a href="/">Projetos</a></li>
-                <li><a href="/">Contato</a></li>
-            </ul>
-        </nav>
-    </header>
-    <!-- Comeco do menu -->
+    <img src="imagens\logo_search.png" alt="logo" class="logo">
     <header class="header">
     <img src="{{ asset('imagens\logo_search.png') }}" alt="Logo" height="300" width="300" draggable="false">
     </header>
